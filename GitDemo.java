c05c4b14991420be4e20316b72712ea1c168985b
VaishnaviRamabhadran <vaishu2395@gmail.com>
2021-02-01 15:52:01 +0530

    Added new file for second commit

--- GitDemo.java
+++ GitDemo.java
@@ -4,5 +4,10 @@ public class GitDemo{
 public static void main(String[] args)
 {
 System.out.println("Hello, this is git repo demo test");
+System.out.println("Now that we have created a new repo, Let's test branching.");
+
+System.out.println("Now that we have created a new repo, Let's test branching.");
+System.out.println("Adding new java file to repo");
+
 }
 }
---
+++ GitDemoClass2.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class GitDemoClass2{
+public static void main(String[] args)
+{
+System.out.println("Hello, this is class2");
+
+}
+}
